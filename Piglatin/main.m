104ea3d3bc0e88e290ab8a56616d483efc3b9025
Kamal Maged <kamal_maged@yahoo.com>
2019-06-11 17:05:40 -0400

    Initial Commit

---
+++ Piglatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Piglatin
+//
+//  Created by Kamal Maged on 2019-06-11.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
